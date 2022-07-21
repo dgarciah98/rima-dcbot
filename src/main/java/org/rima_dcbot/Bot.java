cabfd1226623a2860dfe9c44ea824ab3a69a9057
josel97 <79546389+josel97@users.noreply.github.com>
2022-07-21 20:42:58 +0200

    Add json loader and config util classes

--- src/main/java/org/rima_dcbot/Bot.java
+++ src/main/java/org/rima_dcbot/Bot.java
@@ -5,13 +5,20 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 
+import java.io.IOException;
+
 import javax.security.auth.login.LoginException;
 
+import org.rima_dcbot.loader.JsonLoader;
+
 public class Bot {
-    public static void main(String[] args) throws LoginException {
-        Dotenv dotenv = Dotenv.load();
-        JDA bot = JDABuilder.createDefault(dotenv.get("BOT_TOKEN"))
-                .setActivity(Activity.listening("Lil B - 05 Fuck Em"))
-                .build();
+    public static void main(String[] args) throws LoginException, IOException {
+//        Dotenv dotenv = Dotenv.load();
+//        JDA bot = JDABuilder.createDefault(dotenv.get("BOT_TOKEN"))
+//                .setActivity(Activity.listening("Lil B - 05 Fuck Em"))
+//                .build();
+    	
+    	JsonLoader loader = new JsonLoader();
+    	System.out.println(loader.loadRhymes());
     }
 }
