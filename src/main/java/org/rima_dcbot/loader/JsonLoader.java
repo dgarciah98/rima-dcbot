f9620e3f7fb8ec1358b63d9536063c137eccaf32
josel97 <79546389+josel97@users.noreply.github.com>
2022-07-22 17:48:11 +0200

    Refactor json loader and remove unnecessary throws

--- src/main/java/org/rima_dcbot/loader/JsonLoader.java
+++ src/main/java/org/rima_dcbot/loader/JsonLoader.java
@@ -2,6 +2,8 @@ package org.rima_dcbot.loader;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.rima_dcbot.configuration.ConfigurationUtil;
@@ -13,47 +15,44 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 
 /**
  * JSON file loader
+ * 
  * @author jose
  *
  */
 public class JsonLoader {
-	
+
 	private ObjectMapper om;
 	private ObjectWriter ow;
-	private ConfigurationUtil configUtil;
-	
+
+	private File wordplaysFile;
+	private Map<String, String> wordplays;
+
 	public JsonLoader() {
 		om = new ObjectMapper();
 		ow = om.writer(new DefaultPrettyPrinter() {
-		    @Override
-		    public void writeObjectFieldValueSeparator(JsonGenerator jg) throws IOException {
-		        jg.writeRaw(": ");
-		    }
+			@Override
+			public void writeObjectFieldValueSeparator(JsonGenerator jg) throws IOException {
+				jg.writeRaw(": ");
+			}
 		});
-		configUtil = ConfigurationUtil.getInstance();
+
+		wordplaysFile = Paths.get(ConfigurationUtil.getInstance().getProperty("files.wordplays.path")).toFile();
 	}
-	
+
 	public Map<String, String> loadWordplays() throws IOException {
-		Map<String, String> wordplays = null;
-		
-		try {
-			wordplays = om.readValue(new File(configUtil.getProperty("files.wordplays.path")), Map.class);
-		} catch (IOException e) {
-			System.out.println("Error reading file: " + e);
+		if (wordplays == null) {
+			wordplays = om.readValue(wordplaysFile, Map.class);
 		}
 		
-		return wordplays;
+		// return a copy to avoid unwanted modification of wordplays map
+		// no need to deep copy, because key and values are strings
+		return new HashMap<>(wordplays);
 	}
-	
-	public void saveWordplays(Map<String, String> wordplays) throws IOException {
-		ow.writeValue(new File(configUtil.getProperty("files.wordplays.path")), wordplays);
-	}
-	
+
 	public void addWordplay(String suffix, String wordplay) throws IOException {
-		// TODO find a better way to do this
-		Map<String, String> wordplays = loadWordplays();
-		wordplays.put(suffix, wordplay);
-		saveWordplays(wordplays);
+		// Use loadWordplays instead of direct reference to wordplaysFile in case this is called before loadWordplays is ever called
+		loadWordplays().put(suffix, wordplay);
+		ow.writeValue(wordplaysFile, wordplays);
 	}
-	
+
 }
